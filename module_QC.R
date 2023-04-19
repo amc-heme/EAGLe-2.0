cbfd2f0ba5cb4157a6e470503ffd3af657cdf8b1
srg0502 <srgipson0502@gmail.com>
2023-04-19 13:25:35 -0600

    Update module_QC.R
    added the batch corrected PCA plot and conditionalPanels for selected which plot to load

--- module_QC.R
+++ module_QC.R
@@ -54,23 +54,25 @@ QC_UI <- function(id) {
           right =
             TRUE
         ),
-        # 
-        # conditionalPanel(
-        #   condition = "input.PCAplots == true",
-        #   radioButtons( #choose type of PCA plot
-        #     ns("PCAvar"),
-        #     h4(
-        #       "Choose PCA plot"
-        #     ),
-        #     choices =
-        #       list("VST PCA", "VST + batch corrected PCA"),
-        #     selected =
-        #       "VST PCA"
-        #   )
-        # ),
+
+        conditionalPanel(
+          ns = ns,
+          condition = "input['PCAplots'] == TRUE", 
+          radioButtons( #choose type of PCA plot
+            ns("PCAvar"),
+            h4(
+              "Choose PCA plot"
+            ),
+            choices =
+              list("VST PCA", "VST + batch corrected PCA"),
+            selected =
+              "VST PCA"
+          )
+        ),
         
         conditionalPanel(
-          condition = "input.multiqc == true",
+          ns = ns,
+          condition = "input['multiqc'] == TRUE",
           selectInput( #choose type of multiqc test to visualize
             ns("QCvar"),
             label=
@@ -107,33 +109,50 @@ QC_Server <- function(id, colorpaletteQC) {
     #determine % variance of pc1 and pc2
     pc1var = round(vsd.pca.var[3,1] * 100, 1)
     pc2var = round(vsd.pca.var[3,2] * 100 - pc1var, 1)
+    
+    #batch corrected PCA
+    assay(vsd) <- limma::removeBatchEffect(assay(vsd),
+                                           batch = samples$batch, 
+                                           design = model.matrix(~condition, data = samples))
+    bcvsd.pca <- data.frame(prcomp(t(assay(vsd)))$x) %>% 
+      as_tibble(rownames = "SRR") %>% 
+      left_join(., as_tibble(colData(vsd)))
     #call in color palette server for use in plot
     colorpaletteQC <- 
       paletteServer("palette")
     
-    # PCA_title <- 
-    #   reactive({
-    #     if (input$PCAvar == "VST PCA") {
-    #       print("VST PCA")
-    #     } else if (input$PCAvar == "VST + batch corrected PCA") {
-    #       print("VST + batch corrected PCA")
-    #     }
-    #   })
-    
+    #reactive funtion to choose which PCA plot is loaded
+    PCAdata <-
+      eventReactive(input$PCAvar, {
+        if (input$PCAvar == "VST PCA") {
+          vsd.pca
+        } else if (input$PCAvar == "VST + batch corrected PCA") {
+          bcvsd.pca
+        }
+      })
+    #reactive expression to change the title of the PCA plot based on which PCA is loaded
+    PCA_title <- 
+      reactive({
+        if (input$PCAvar == "VST PCA") {
+          print("VST PCA")
+        } else if (input$PCAvar == "VST + batch corrected PCA") {
+          print("VST + batch corrected PCA")
+        }
+      })
     output$PCAplot <- renderPlot ({
      if(input$PCAplots == TRUE) {
-     pca <- ggplot(vsd.pca, aes(x = PC1, y = PC2, shape = condition, color = batch, fill = batch)) + 
+     pca <- ggplot(PCAdata(), aes(x = PC1, y = PC2, shape = condition, color = batch, fill = batch)) + 
         geom_point(size = 5) + 
         scale_shape_manual(values = c(21, 24), name = '') +
-        scale_fill_viridis_d(option = colorpaletteQC()) + #scale_fill_manual reactive function
-        scale_color_viridis_d(option = colorpaletteQC()) + #scale_color manual reactive function
+        scale_fill_viridis_d(option = "viridis") + #scale_fill_manual reactive function
+        scale_color_viridis_d(option = "viridis") + #scale_color manual reactive function
         theme_cowplot(font_size = 18) + 
         theme(axis.title = element_text(face = "bold"), title = element_text(face = "bold")) +
         theme(plot.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF")) +
         theme(panel.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF")) +
         xlab(paste('PC1 =', pc1var, '% variance')) +
         ylab(paste('PC2 =', pc2var, '% variance')) +
-        #ggtitle(PCA_title()) + #reactive title
+        ggtitle(PCA_title()) + #reactive title
         geom_text_repel(colour = "black", aes(label=sample_name),hjust=0, vjust=0)
       print(pca)
      }
