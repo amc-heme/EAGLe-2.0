0cdf330518418827adf52b2e151374473ee2c9a2
srg0502 <srgipson0502@gmail.com>
2023-10-05 14:01:55 -0600

    add additional dds files

--- config_CD.R
+++ config_CD.R
@@ -1,28 +1,46 @@
 
-#   num_PC = nrow(metadata),
-#   var_1 = samples$condition,
-#   var_2 = samples$batch)
-
-globalDataServer <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    
-    # list(
-    #   vsd = reactive({readRDS("data_rds_files/CD_ROSlow_vsd.rds")}),
-    #   vsd.pca = reactive({readRDS("data_rds_files/CD_ROSlow_vsd.pca.rds")})
-    # )
-    stash = reactiveValues()
-    stash$t2g = readRDS("data_rds_files/t2g_hs.rds")
-    #stash$metadata = readRDS("data_rds_files/CD_ROSlow_metadata.rds")
-    stash$dds = readRDS("data_rds_files/CD_ROSlow_dds.rds")
-    #stash$dds_res = readRDS("data_rds_files/CD_ROSlow_dds.res.rds")
-    stash$vsd = readRDS("data_rds_files/CD_ROSlow_vsd.rds")
-    #stash$vst = readRDS("data_rds_files/CD_vst_batchcorrected.rds")
-    #stash$vsd.pca = readRDS("data_rds_files/CD_ROSlow_vsd.pca.rds")
-    #stash$vst.goi = readRDS("data_rds_files/CD_ROSlow_vst.goi.rds")
-    stash$qc = "data/multiqc_data.json"
-    return(stash)
-    # return(list(get_t2g = reactive(stash$t2g), get_metadata = reactive(stash$metadata), get_dds = reactive(stash$dds),
-    #             get_dds.res = reactive(stash$dds_res), get_vsd = reactive(stash$vsd), get_vst = reactive(stash$vst),
-    #             get_vsd.pca = reactive(stash$vsd.pca), get_vst.goi = reactive(stash$vst.goi), get_qc = reactive(stash$qc)))
-  })
-}
+# globalDataServer <- function(id) {
+#   moduleServer(id, function(input, output, session) {
+# 
+#     stash = reactiveValues()
+#     stash$t2g = readRDS("data_rds_files/t2g_hs.rds") 
+#     #stash$metadata = readRDS("data_rds_files/CD_ROSlow_metadata.rds")
+#     stash$dds = readRDS("data_rds_files/CD_ROSlow_dds.rds")
+#     #stash$dds_res = readRDS("data_rds_files/CD_ROSlow_dds.res.rds")
+#     stash$vsd = readRDS("data_rds_files/CD_ROSlow_vsd.rds")
+#     #stash$vst = readRDS("data_rds_files/CD_vst_batchcorrected.rds")
+#     #stash$vsd.pca = readRDS("data_rds_files/CD_ROSlow_vsd.pca.rds")
+#     #stash$vst.goi = readRDS("data_rds_files/CD_ROSlow_vst.goi.rds")
+#     stash$qc = "data/multiqc_data.json"
+#     
+#     # return(list(get_t2g = reactive(stash$t2g), get_metadata = reactive(stash$metadata), get_dds = reactive(stash$dds),
+#     #             get_dds.res = reactive(stash$dds_res), get_vsd = reactive(stash$vsd), get_vst = reactive(stash$vst),
+#     #             get_vsd.pca = reactive(stash$vsd.pca), get_vst.goi = reactive(stash$vst.goi), get_qc = reactive(stash$qc)))
+#   })
+# }
+Datasets:
+  Cancer_Discovery:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/CD_ROSlow_dds.rds"
+    species: human
+  Ye_16:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/Ye_16_dds.rds"
+    species: mouse
+  Ye_20:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/Ye_20_dds.rds"
+    species: mouse
+  Venaza:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/venaza_dds.rds"
+    species: human
+  Lagadinou: 
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/EL_AML_dds.rds"
+    species: human
+  Lee:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/Lee_dds.rds"
+    species: human
+  BEAT:
+    path: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/BEAT_AML_dds.rds"
+    species: human
+Transcript_to_gene:
+  mouse: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/t2g_mm.rds"
+  human: "~/Documents/GitHub/EAGLe-2.0/data_rds_files/t2g_hs.rds"
+  
--- module_data.R
+++ module_data.R
@@ -15,16 +15,14 @@ data_UI <- function(id) {
       sidebarPanel(
           selectInput(
             ns("datainput"), label = NULL,
-             choices = c("Pei et al, 2020" = "CancerDiscovery",
-                         "Ye et al, 2016" = "Ye16",
-                         "Ye et al, 2020" = "Ye20",
-                         "Pollyea Ven/Aza, 2018" = "venaza",
-                         "Lagadiou, 2013" = "stemcell",
-                         "TCGA-LAML" = "tcga",
-                         "BEAT-AML" = "beat",
-                         "Target pediatric AML" = "target",
-                         "Human Protein Atlas" = "hpa",
-                         "Lee et al, 2018" = "lee"), selected = NULL
+             choices = c("Pei et al, 2020" = "Cancer_Discovery",
+                         "Ye et al, 2016" = "Ye_16",
+                         "Ye et al, 2020" = "Ye_20",
+                         "Pollyea Ven/Aza, 2018" = "Venaza",
+                         "Lagadiou, 2013" = "Lagadinou",
+                         "TCGA-LAML" = "TCGA",
+                         "BEAT-AML" = "BEAT",
+                         "Lee et al, 2018" = "Lee"), selected = NULL
           )
       ),
       mainPanel(
