59f387007a3d129b8e03a3b7462f651dcfd2217e
srg0502 <srgipson0502@gmail.com>
2023-12-08 12:10:43 -0700

    moved all modules into R folder

---
+++ R/module_singlecolor_palette.R
@@ -0,0 +1,40 @@
+# library(shiny) 
+# library(colourpicker)
+# library(esquisse)
+# 
+# colorUI <- function(id, label, value) {
+#   ns <- NS(id)
+#   
+#   colourInput(
+#     ns("colorpalette"),
+#     label = label,
+#     value = value,
+#     showColour = ("both"),
+#     palette = ("square"),
+#     allowedCols = NULL,
+#     allowTransparent = FALSE,
+#     returnName = FALSE,
+#     closeOnClick = FALSE
+#   )
+# }
+# 
+# colorServer <- function(id) {
+#   moduleServer(id, function(input, output, session) {
+#     singlecolor <- reactive({
+#       print("value of color palette within color module")
+#       print(input$colorpallete)
+#       input$colorpalette
+#     })
+#     
+#     return(singlecolor)
+#     
+#   })
+# }
+# colorApp <- function() {
+#   ui <- fluidPage(colorUI("color"))
+#   server <- function(input, output, session) {
+#     colorServer("color")
+#   }
+#   shinyApp(ui, server)
+# }
+# colorApp()
